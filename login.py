88362bf42c3bf05978f2130f1702ac0ef9ccb26c
Quinjan Robert Ocampo <quinjanrobertocampo@gmail.com>
2018-08-26 14:50:45 +0800

    Template File added

---
+++ login.py
@@ -0,0 +1,19 @@
+usernames = {}
+passwords = {}
+names = {}
+flag = 0
+
+def displayMenu():
+    # do something
+    return
+
+def reader():
+    # do something
+    return
+def checker(username, password):
+    # do something
+    return 3
+
+while flag != 1:
+    displayMenu()
+
