c2c231a8574b0704dfa6220d211b6231ba50f10a
MarlonNeu <42406572+MarlonNeu@users.noreply.github.com>
2018-08-26 18:13:45 +0800

    commit

--- login.py
+++ login.py
@@ -1,13 +1,3 @@
-import csv
-import argparse
-import os
-os.getcwd()
-parser = argparse.ArgumentParser()
-parser.add_argument("username", help = "Input the Username")
-parser.add_argument("password", help = "Input the Password")
-args = parser.parse_args()
-users = []
-
 def read():
     with open("D:/MAPUA/2018-2019/COE125/login/accounts.csv") as csvfile:
         reader = csv.DictReader(csvfile)
@@ -15,18 +5,3 @@ def read():
             users.append(dict(username=row['username'],
                                  password=row['password'],
                                  name=row['name']))
-      
-def login():
-    username = args.username
-    password = args.password
-    for user in users:
-        if user['username'] == username and user['password'] == password:
-            print('You are logged in.')
-            break
-        else:
-            print('Wrong input. Try Again')
-            break
-    
-# ---- Main ---- #
-read()
-login()
