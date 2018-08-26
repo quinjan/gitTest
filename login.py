38340fb667efc1091abd782d28b609fcaea5fc64
Ian Gabrielle Bautista <42402512+iangabrielle@users.noreply.github.com>
2018-08-26 18:02:33 +0800

    Changes Made

--- login.py
+++ login.py
@@ -1,21 +1,4 @@
-import csv
-import argparse
-import os
-os.getcwd()
-parser = argparse.ArgumentParser()
-parser.add_argument("username", help = "Input the Username")
-parser.add_argument("password", help = "Input the Password")
-args = parser.parse_args()
-users = []
-
-def read():
-    with open("D:/MAPUA/2018-2019/COE125/login/accounts.csv") as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            users.append(dict(username=row['username'],
-                                 password=row['password'],
-                                 name=row['name']))
-      
+     
 def login():
     username = args.username
     password = args.password
@@ -26,7 +9,3 @@ def login():
         else:
             print('Wrong input. Try Again')
             break
-    
-# ---- Main ---- #
-read()
-login()
